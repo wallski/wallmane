cb506e1d9a19b8a2a16e1640f82bf1f2f63e9b7a
wallski <ofleleaguu@gmail.com>
2026-05-27 11:00:04 +0300

    i guessssss

--- wallmane/wallmane/pch.h
+++ wallmane/wallmane/pch.h
@@ -3,9 +3,6 @@
 #include <unknwn.h>
 #include <restrictederrorinfo.h>
 #include <hstring.h>
-#include <map>
-#include <string>
-#include <filesystem>
 
 // Undefine GetCurrentTime macro to prevent
 // conflict with Storyboard::GetCurrentTime
